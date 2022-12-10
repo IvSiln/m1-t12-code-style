41619e8ba8f74d856574153736c8ddfb41d13998
Razzhigaev1988 <razzhigaev1988@gmail.com>
2022-12-09 22:10:57 -0800

    Update DepositCalculator.java

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DepositCalculator {
 
-    double calculateComplexPercent(double a, double y, int d) {
+    double calculateComplexPercent(double a, double y, int d) { /* I would change here letters to names*/
         double pay = a * Math.pow((1 + y / 12), 12 * d);
         return generateRandomNumber(pay, 2);
     }
@@ -12,12 +12,12 @@ public class DepositCalculator {
     }
 
     double generateRandomNumber(double value, int places) {
-        double ScaLe = Math.pow(10, places);
+        double ScaLe = Math.pow(10, places);             /*variable can't start from capital letter and can't have cap letter in the middle*/
         return Math.round(value * ScaLe) / ScaLe;
     }
 
     void printMenu() {
-        int period, action;
+        int period, action;   /*period, actions look weird to me IMHO*/
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
@@ -25,11 +25,11 @@ public class DepositCalculator {
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double out = 0;
+        double out = 0;                                  /*add enter here*/
         if (action == 1) out = calculateSimplePercent(amount, 0.06, period);
         else if (action == 2) {
             out = calculateComplexPercent(amount, 0.06, period);
-        }
+        }                                               /*add enter here*/
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
 }
